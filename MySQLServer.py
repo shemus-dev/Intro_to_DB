58374a9d4a63ec574b4c15717eb98d237478e9e7
shemus-dev <shemmutuku4@gmail.com>
2025-10-19 23:20:54 +0300

    Add alx_book_store.sql database schema

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,13 +1,13 @@
+
 import mysql.connector
-from mysql.connector import Error
 
 def create_database():
     try:
         # Connect to MySQL Server
         connection = mysql.connector.connect(
             host="localhost",
-            user="root",          # change this to your MySQL username (e.g., 'navin' or 'shem')
-            password="root"       # change this to your MySQL password
+            user="root",          # change this to your MySQL username (e.g., 'shem' or 'navin')
+            password="1234"       # change this to your MySQL password
         )
 
         if connection.is_connected():
@@ -17,15 +17,18 @@ def create_database():
             cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
             print("Database 'alx_book_store' created successfully!")
 
-    except Error as e:
+    except mysql.connector.Error as e:
         print(f"Error while connecting to MySQL: {e}")
 
     finally:
         # Close connection properly
-        if connection.is_connected():
-            cursor.close()
-            connection.close()
-            print("MySQL connection is closed")
+        try:
+            if connection.is_connected():
+                cursor.close()
+                connection.close()
+                print("MySQL connection is closed")
+        except:
+            pass
 
 # Run the function
 if __name__ == "__main__":
